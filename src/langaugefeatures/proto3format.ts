17a7c490773645bd409da825f1a968df0906480f
Ukjae Jeong (Jay) <jeongukjae@gmail.com>
2023-02-20 23:06:41 +0900

    add clang-format formatter

---
+++ src/langaugefeatures/proto3format.ts
@@ -0,0 +1,39 @@
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
+import { isCommandAvailable, isExecutableFileAvailable } from '../utils';
+
+export const proto3FormatProvider: vscode.DocumentFormattingEditProvider = {
+    provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.TextEdit[] {
+        const formatOption = vscode.workspace.getConfiguration('protobuf.format');
+        if (formatOption.get('provider', "clang-format") === "clang-format") {
+            return formatUsingClangFormat(document);
+        }
+
+        vscode.window.showErrorMessage(`Unknown format provider: ${formatOption.get('provider')}`);
+        return [];
+    },
+};
+
+function formatUsingClangFormat(document: vscode.TextDocument): vscode.TextEdit[] {
+    const clangFormatOption = vscode.workspace.getConfiguration('protobuf.clang-format');
+    const clangFormatPath = clangFormatOption.get('executable', 'clang-format');
+
+    if (!(isCommandAvailable(clangFormatPath) || isExecutableFileAvailable(clangFormatPath))) {
+        vscode.window.showErrorMessage(`clang-format executable not found: ${clangFormatPath}`);
+        return [];
+    }
+
+    // clang-format assumes the filename to determine the language.
+    let args = [
+        '--assume-filename=a.proto',
+    ];
+    clangFormatOption.get('arguments', []).forEach((arg: string) => {
+        args.push(arg);
+    });
+
+    let formatResult = cp.execFileSync(clangFormatPath, args, {input: document.getText()});
+    return [new vscode.TextEdit(
+        new vscode.Range(0, 0, document.lineCount, document.lineAt(document.lineCount - 1).text.length),
+        formatResult.toString(),
+    )];
+}
---
+++ src/utils.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs';
+import * as os from 'os';
+import * as cp from 'child_process';
+
+export function isCommandAvailable(command: string): boolean {
+	if (os.platform().indexOf('win') === 0) {
+		try {
+			cp.execSync(`where ${command}`);
+			return true;
+		} catch (err) {
+			return false;
+		}
+	} else {
+		try {
+			cp.execSync(`which ${command}`);
+			return true;
+		} catch (err) {
+			return false;
+		}
+	}
+}
+
+export function isExecutableFileAvailable(filePath: string): boolean {
+	try {
+		fs.accessSync(filePath, fs.constants.F_OK | fs.constants.X_OK);
+		return true;
+	} catch (err) {
+		return false;
+	}
+}
