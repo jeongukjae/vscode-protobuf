2813a096610bf7ce31934eb42716491f74debded
Ukjae Jeong (Jay) <jeongukjae@gmail.com>
2023-04-04 00:09:17 +0900

    update suite test case

--- src/test/suite/proto3Symbol.test.ts
+++ src/test/suite/proto3Symbol.test.ts
@@ -20,4 +20,24 @@ suite("Proto3 Symbol Provider", () => {
         expect(symbols[0].kind).to.equal(vscode.SymbolKind.Class);
       });
   });
+
+  test("Should provide field type", () => {
+    vscode.workspace
+      .openTextDocument({
+        language: "proto3",
+        content: "message Foo { float a = 1; ; ; ; }",
+      })
+      .then((doc) => {
+        const symbols = proto3SymbolProvider.provideDocumentSymbols(
+          doc,
+          new vscode.CancellationTokenSource().token
+        ) as vscode.SymbolInformation[];
+
+        expect(symbols.length).to.equal(2);
+        expect(symbols[0].name).to.equal("Foo");
+        expect(symbols[0].kind).to.equal(vscode.SymbolKind.Class);
+        expect(symbols[1].name).to.equal("a");
+        expect(symbols[1].kind).to.equal(vscode.SymbolKind.Field);
+      });
+  });
 });
