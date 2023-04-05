8a18678fb00784a424f67093ffbc713eb1c128f6
Jeong Ukjae <jeongukjae@gmail.com>
2023-04-05 17:42:39 +0900

    fixlint

--- src/test/unit/parser/tokenstream.test.ts
+++ src/test/unit/parser/tokenstream.test.ts
@@ -3,43 +3,43 @@ import { expect } from "chai";
 import { TokenStream } from "../../../parser/tokenstream";
 
 class TestStreamUnit {
-    start: number;
-    length: number;
+  start: number;
+  length: number;
 
-    constructor(start: number, length: number) {
-        this.start = start;
-        this.length = length;
-    }
+  constructor(start: number, length: number) {
+    this.start = start;
+    this.length = length;
+  }
 }
 
 describe("TokenStream", () => {
-    it("should return the correct token text", () => {
-        const st = new TokenStream("abc", [
-            new TestStreamUnit(0, 1),
-            new TestStreamUnit(1, 1),
-            new TestStreamUnit(2, 1),
-        ]);
+  it("should return the correct token text", () => {
+    const st = new TokenStream("abc", [
+      new TestStreamUnit(0, 1),
+      new TestStreamUnit(1, 1),
+      new TestStreamUnit(2, 1),
+    ]);
 
-        expect(st.getCurrentTokenText()).to.equal("a");
-        expect(st.getNextTokenText()).to.equal("b");
-        expect(st.lookAheadText(2)).to.equal("c");
-    });
+    expect(st.getCurrentTokenText()).to.equal("a");
+    expect(st.getNextTokenText()).to.equal("b");
+    expect(st.lookAheadText(2)).to.equal("c");
+  });
 
-    it("should detect end of stream", () => {
-        const st = new TokenStream("abc", [
-            new TestStreamUnit(0, 1),
-            new TestStreamUnit(1, 1),
-            new TestStreamUnit(2, 1),
-        ]);
+  it("should detect end of stream", () => {
+    const st = new TokenStream("abc", [
+      new TestStreamUnit(0, 1),
+      new TestStreamUnit(1, 1),
+      new TestStreamUnit(2, 1),
+    ]);
 
-        expect(st.isEndOfStream()).to.be.false;
+    expect(st.isEndOfStream()).to.be.false;
 
-        st.moveNext();
+    st.moveNext();
 
-        expect(st.isEndOfStream()).to.be.false;
+    expect(st.isEndOfStream()).to.be.false;
 
-        st.moveNext();
+    st.moveNext();
 
-        expect(st.isEndOfStream()).to.be.true;
-    });
+    expect(st.isEndOfStream()).to.be.true;
+  });
 });
