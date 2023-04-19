7e243d8ea1782c4506d1f7124ed9d68e5633b6a8
Jeong Ukjae <jeongukjae@gmail.com>
2023-04-19 14:37:55 +0900

    add node types

---
+++ src/parser/textproto/nodes.ts
@@ -0,0 +1,38 @@
+export const enum NodeType {
+  invalid,
+  empty,
+
+  document,
+
+  comment,
+  header, // proto-file: and proto-message: directives
+
+  message,
+  field,
+}
+
+export abstract class Node {
+  type: NodeType;
+  start: number;
+  end: number;
+
+  parent?: Node;
+  children?: Node[];
+
+  constructor(type: NodeType, start: number, end: number) {
+    this.type = type;
+    this.start = start;
+    this.end = end;
+  }
+
+  setParent(node: Node) {
+    this.parent = node;
+  }
+
+  add(node: Node) {
+    if (!this.children) {
+      this.children = [];
+    }
+    this.children.push(node);
+  }
+}
