1ace33ab5ccb1abc371942cee20563b5a34403e2
Jeong Ukjae <jeongukjae@gmail.com>
2023-05-03 15:47:16 +0900

    add test case for textproto format

---
+++ src/test/languagefeatures/textproto/format.test.ts
@@ -0,0 +1,51 @@
+import { expect } from "chai";
+import * as vscode from "vscode";
+
+import { textprotoFormatProvider } from "../../../langaugefeatures/textproto/format";
+import { isCommandAvailable } from "../../../utils";
+
+suite("LanguageFeatrues >> TextProto >> Format", () => {
+  vscode.window.showInformationMessage("Start TextProto Format tests.");
+
+  test("should be able to format temp with txtpbfmt", async () => {
+    if (!isCommandAvailable("txtpbfmt")) {
+      throw new Error(
+        "txtpbfmt is not available. " +
+          "Please install it and run this test again."
+      );
+    }
+
+    let settings = vscode.workspace.getConfiguration("textproto");
+    settings.update("format.provider", "txtpbfmt");
+    settings.update("txtpbfmt.executable", "txtpbfmt");
+
+    // no needs to check all format options.
+    // just checking whether it works or not.
+    let doc = await vscode.workspace.openTextDocument({
+      language: "textproto",
+      content: `foo: "bar"
+
+
+
+      bar: "baz"
+`,
+    });
+
+    await vscode.window.showTextDocument(doc);
+
+    let edits = await textprotoFormatProvider.provideDocumentFormattingEdits(
+      doc,
+      {
+        tabSize: 2,
+        insertSpaces: true,
+      },
+      new vscode.CancellationTokenSource().token
+    );
+
+    expect(edits).to.be.lengthOf(1);
+    expect(edits![0].newText).to.be.equal(`foo: "bar"
+
+bar: "baz"
+`);
+  });
+});
